85b795d9a3a534fb97f75bb413791ff8cb93adf6
Sergey Bednyakov <sbednyakov@gmail.com>
2022-08-01 06:20:33 +0300

    Add project files.

---
+++ AdapterRealExample/CanvasPainterToPrinterAdapter.cs
@@ -0,0 +1,19 @@
+﻿using AdapterRealExample.Devices;
+
+namespace AdapterRealExample
+{
+    public  class CanvasPainterToPrinterAdapter : IPrinter
+    {
+        private CanvasPainter _canvasPainter;
+        public CanvasPainterToPrinterAdapter(CanvasPainter canvasPainter)
+        {
+            _canvasPainter = canvasPainter;
+        }
+
+        public void Print()
+        {
+            _canvasPainter.Paint();
+        }
+
+    }
+}
---
+++ AdapterRealExample/Devices/IPainter.cs
@@ -0,0 +1,11 @@
+﻿namespace AdapterRealExample.Devices
+{
+    /// <summary>
+    /// Интерфейс устройства, печатающего на холсте
+    /// </summary>
+    interface IPainter
+    {
+        void Paint();
+    }
+}
+  
